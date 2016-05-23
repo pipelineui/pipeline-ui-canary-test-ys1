2d10e00096f1d683205789fbad4107c17dd52bb9
pipelineui <pipelineui@gmail.com>
2016-05-23 08:26:02 -0400

    Update spec.js

--- spec.js
+++ spec.js
@@ -1,6 +1,8 @@
+var expect = require("expect");
+
 // Dummy mocha test for canary test
 describe("Dummy test", function () {
-    it("should always pass", function (done) {
+    it("should always pass", function() {
         expect(true).to.equal(true);
     });
 });
